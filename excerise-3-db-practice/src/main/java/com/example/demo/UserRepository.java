de7535f970fafe8d4131b225ff600a777d100fe7
Kaichi Iwahara <umichi1222@gmail.com>
2024-07-15 22:09:20 +0900

    [fix]SQL error

--- excerise-3-db-practice/src/main/java/com/example/demo/UserRepository.java
+++ excerise-3-db-practice/src/main/java/com/example/demo/UserRepository.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-	
+public interface UserRepository extends JpaRepository<User, Long>{
+
 }
